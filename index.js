3a832c79292c5463f256bfb721a7f0c97465487d
IroncladDev <>
2023-10-27 00:28:12 +0200

    first commit to git

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
 import generateResponse from "./lib/generateResponse.js";
 import promptSync from 'prompt-sync';
-
-const prompt = promptSync();
-
+const app = express();
+app.use(bodyParser.json());
 const conversationHistory = [];
+app.post('/ask', async (req, res) => {
+  const question = req.body.question;
+  const answer = await generateResponse({
+    prompt: question,
+    history: conversationHistory
+  });
 
+  res.json({ answer });
+  conversationHistory.push(`Human: ${question}`, `Amjad Masad: ${answer}`);
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+const prompt = promptSync();
+let question;
 while (true) {
-  const question = prompt("Ask a question >");
+  question = prompt("Ask a question >");
   const answer = await generateResponse({
     prompt: question,
     history: conversationHistory
   });
 
   console.log(`Amjad Masad: ${answer}\n`);
-  
+
   conversationHistory.push(`Human: ${question}`, `Amjad Masad: ${answer}`)
 }
+
+
+
